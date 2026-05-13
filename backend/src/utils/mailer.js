218a3e4b5b6e8652614bb2f7a6139dd2a70794a0
202301091 <146870562+202301091@users.noreply.github.com>
2026-05-13 12:43:13 +0530

    Solve the error in backend deployment

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -1,7 +1,5 @@
 import nodemailer from "nodemailer";
 
-import nodemailer from "nodemailer";
-
 export async function sendWithGmail(to, otp) {
   try {
     const transporter = nodemailer.createTransport({
